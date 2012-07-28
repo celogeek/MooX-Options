1a235ef11a6553381443683e9e3bb2558892dc0a
celogeek <me@celogeek.com>
2012-07-29 00:42:30 +0200

    Full rewrite of import in MooX::Options and use role
    Role will declared all method and MooX::Options will wrap the
    option_information that will be attached to the current package. It pass
    it to the role with 'around' method.

--- lib/MooX/Options.pm
+++ lib/MooX/Options.pm
@@ -13,214 +13,27 @@ You will have "new_with_options" to instanciate new object for command line.
 
 use strict;
 use warnings;
-
-# VERSION
 use Carp;
 use Data::Dumper;
-use Getopt::Long 2.38;
-use Getopt::Long::Descriptive 0.091;
-use Regexp::Common;
-use Data::Record;
 
-my %DEFAULT_OPTIONS = (
-    'creation_chain_method' => 'new',
-    'creation_method_name'  => 'new_with_options',
-    'option_chain_method'   => 'has',
-    'option_method_name'    => 'option',
-    'flavour'               => [],
-    'protect_argv'          => 1,
-);
+# VERSION
 
-my @FILTER = qw/format short repeatable negativable autosplit doc/;
-
-## no critic qw(ProhibitExcessComplexity)
 sub import {
-    my ( undef, %import_params ) = @_;
-    my (%import_options) = ( %DEFAULT_OPTIONS, %import_params );
-    my $package = caller;
+  caller->can('with')->('MooX::Options::Role');
 
-    #check options and definition
-    while ( my ( $key, $method ) = each %import_options ) {
-        croak "missing option $key, check doc to define one"
-            unless defined $method;
-        croak "method $method is not defined, check doc to use another name"
-            if $key =~ /_chain_method$/x && !$package->can($method);
-        croak "method $method already defined, check doc to use another name"
-            if $key =~ /_method_name$/x && $package->can($method);
-    }
+  my $option_information = {};
+  
+  caller->can('around')->(option_information => sub {
+      my ($orig,$self) = (shift, shift);
+      return ($self->$orig(@_), %$option_information);
+  });
 
-    my @Options              = ('USAGE: %c %o');
-    my @Attributes           = ();
-    my @Required_Attributes  = ();
-    my %Autosplit_Attributes = ();
-    my $Usage                = "";
+  caller->can('around')->('option' => sub {
+      my $orig = shift;
+      return $orig->($option_information, @_);
+  });
 
-    my $sub_ref = {};
-
-    #keywords option
-    $sub_ref->{ $import_options{option_method_name} } = sub {
-        my ( $name, %orig_options ) = @_;
-        my %options = %orig_options;
-        croak
-            "Negativable params is not usable with non boolean value, don't pass format to use it !"
-            if $options{negativable} && $options{format};
-        croak "Can't use option with help, it is implied by MooX::Options"
-            if $name eq 'help';
-        croak "Can't use option with "
-            . $import_options{option_method_name}
-            . "_usage, it is implied by MooX::Options"
-            if $name eq $import_options{option_method_name}
-            . "_usage";
-
-        #fix missing option, autosplit implie repeatable
-        $options{repeatable} = 1 if $options{autosplit};
-
-        #help is use for help message only
-        my $name_long_and_short = join "|", grep { defined $_ } $name,
-            $options{short};
-
-        #fix format for negativable or add + if it is a boolean
-        if ( $options{repeatable} ) {
-            if ( $options{format} ) {
-                $options{format} .= "@"
-                    unless substr( $options{format}, -1 ) eq '@';
-            }
-            else {
-                $name_long_and_short .= "+";
-            }
-        }
-
-        #negativable for boolean value
-        $name_long_and_short .= "!" if $options{negativable};
-
-        #format the name
-        my $name_format = join "=",
-            grep { defined $_ } $name_long_and_short, $options{format};
-
-        #doc
-        my $doc
-            = defined $options{doc}           ? $options{doc}
-            : defined $options{documentation} ? $options{documentation}
-            :                                   "no doc for $name";
-
-        push @Options, [ $name_format, $doc ];    # prepare option for getopt
-        push @Attributes,          $name;   # save the attribute for later use
-        push @Required_Attributes, $name
-            if $options{required};          # save the required attribute
-        $Autosplit_Attributes{$name}
-            = Data::Record->new(
-            { split => $options{autosplit}, unless => $RE{quoted} } )
-            if $options{autosplit};         # save autosplit value
-
-#remove bad key for passing to chain_method(has), avoid warnings with Moo/Moose
-#by defaut, keep all key
-        unless ( $import_options{nofilter} ) {
-            delete $orig_options{$_} for @FILTER;
-        }
-
-        #chain to chain_method (has)
-        my $chain_method_name = $import_options{option_chain_method};
-        ## no critic qw(ProhibitStringyEval)
-        my $chain_method
-            = eval "package ${package}; sub {${chain_method_name}(\@_)}";
-        ## use critic
-        $chain_method->( $name, %orig_options );
-    };
-
-    #keyword option_usage
-    $sub_ref->{ $import_options{option_method_name} . "_usage" } = sub {
-        my ( $code, @messages ) = @_;
-        print join( "\n", @messages, $Usage );
-        exit($code);
-    };
-
-    #keyword new_with_options
-    $sub_ref->{ $import_options{creation_method_name} } = sub {
-        my ( $self, %params ) = @_;
-
-        #ensure all method will be call properly
-        for my $attr (@Attributes) {
-            croak "attribute "
-                . $attr
-                . " isn't defined. You have something wrong in your option_chain_method '"
-                . $import_options{option_chain_method} . "'."
-                unless $self->can($attr);
-        }
-
-#if autosplit attributes is present, search and replace in ARGV with full version
-#ex --test=1,2,3 become --test=1 --test=2 --test=3
-        ## no critic qw(RequireLocalizedPunctuationVars)
-        local @ARGV = @ARGV if $import_options{protect_argv};
-        if (%Autosplit_Attributes) {
-            my @new_argv;
-
-            #parse all argv
-            for my $arg (@ARGV) {
-                my ( $arg_name, $arg_values ) = split( /=/x, $arg, 2 );
-                $arg_name =~ s/^--?//x;
-                if ( my $rec = $Autosplit_Attributes{$arg_name} ) {
-                    foreach my $record ( $rec->records($arg_values) ) {
-
-                        #remove the quoted if exist to chain
-                        $record =~ s/^['"]|['"]$//gx;
-                        push @new_argv, "--$arg_name=$record";
-                    }
-                }
-                else {
-                    push @new_argv, $arg;
-                }
-            }
-            @ARGV = @new_argv;
-        }
-        ## use critic
-
-        #call describe_options
-        my $usage_method
-            = $self->can("$import_options{option_method_name}_usage");
-        my ( $opt, $usage ) = describe_options(
-            @Options,
-            [ "help|h", "show this help message" ],
-            { getopt_conf => $import_options{flavour} }
-        );
-        $Usage = $usage->text;
-        $usage_method->(0) if $opt->help;
-
-        #replace command line attribute in params if params not defined
-        my @existing_attributes = grep {
-            my $attr     = $_;
-            my $attr_val = $opt->$attr;
-            defined $attr_val && !exists $params{$attr}
-        } @Attributes;
-        @params{@existing_attributes} = @$opt{@existing_attributes};
-
-        #check required params, if anything missing, display help
-        my @missing_params
-            = grep { !defined $params{$_} } @Required_Attributes;
-        $usage_method->( 1, map {"$_ is missing"} @missing_params )
-            if @missing_params;
-
-        my $creation_method_name = $import_options{creation_chain_method};
-        my $creation_method      = $package->can($creation_method_name);
-        $creation_method->( $self, %params );
-    };
-
-    #inject method
-    {
-        ## no critic qw(ProhibitNoStrict)
-        no strict qw/refs/;
-        for my $meth ( keys %$sub_ref ) {
-            *{"${package}::$meth"} = $sub_ref->{$meth};
-        }
-        ## use critic
-
-        #Save option name for MooX::Options::Role
-        ## no critic qw(ProhibitPackageVars)
-        ${"${package}::_MooX_Options_Option_Name"}
-            = $import_options{option_method_name};
-        ## use critic
-    }
-
-    return;
+  return;
 }
 
 1;
--- lib/MooX/Options/Role.pm
+++ lib/MooX/Options/Role.pm
@@ -1,80 +1,42 @@
 package MooX::Options::Role;
 
-# ABSTRACT: create role with option
+# ABSTRACT: role that is apply to your object
 use strict;
 use warnings;
-
+use feature 'state';
 # VERSION
+
+=head1 TODO
+
+Write a doc
+
+=cut
+
+use MRO::Compat;
+use Moo::Role;
+
 use Carp;
+use Data::Dumper;
 
-=head1 SYNOPSIS
-
-    use strict;
-    use warnings;
-    use v5.16;
-    {package myRole;
-     use MooX::Options::Role;
-     option 'multi' => (is => 'rw', doc => 'multi threaded mode');
-     1;
-    }
-    {package myOpt;
-    use Moo;
-    use MooX::Options;
-    myRole->import;
-    1;
-    }
-
-    my $opt = myOpt->new_with_options();
-    say "Multi : ",$opt->multi;
-
-
-You take a look at t/role.t for more example.
-
-=cut
-
-=method import
-Import method "option" that will be transmit to L<MooX::Options> when the role is used.
-
-If you decide to change the "option" key in the import of L<MooX::Options>, L<MooX::Options::Role> will know and call the appropriate method.
-=cut
-
-sub import {
-    ## no critic qw(ProhibitPackageVars)
-    my $package_role         = caller;
-    my $package_role_options = do {
-        ## no critic qw(ProhibitNoStrict)
-        no strict qw/refs/;
-        ${"${package_role}::_MooX_Options_Option_Spec"} //= {};
-    };
-
-    my $option_role_meth = sub {
-        my ( $name, %options ) = @_;
-        $package_role_options->{$name} = \%options;
-    };
-
-    my $import_meth = sub {
-        my $package          = caller;
-        my $option_meth_name = do {
-            ## no critic qw(ProhibitNoStrict)
-            no strict qw/refs/;
-            ${"${package}::_MooX_Options_Option_Name"};
-        };
-        croak "MooX::Options should be import before using this role."
-            unless defined $option_meth_name;
-        my $option_meth = $package->can($option_meth_name);
-        for my $name ( keys %$package_role_options ) {
-            my %option = %{ $package_role_options->{$name} };
-            $option_meth->( $name, %option );
-        }
-    };
-
-    {
-        ## no critic qw(ProhibitNoStrict)
-        no strict qw/refs/;
-        *{"${package_role}::option"} = $option_role_meth;
-        *{"${package_role}::import"} = $import_meth;
-    }
-    return;
+sub new_with_options {
+  my $class = shift;
+  $class->new($class->parse_options(@_));
 }
 
+sub parse_options {
+  my %option_info = shift->option_information;
+  carp Dumper \%option_info;
+  return;
+}
+
+sub option_information {
+  shift->maybe::next::method(@_);
+}
+
+sub option {
+    my ($option_information, $name, %attributes) = @_;
+    has $name => %attributes;
+    $option_information->{$name} = { %attributes };
+    return;
+}
 1;
