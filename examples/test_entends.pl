3bbe2a0847f21a1d81fe33f02ef04dad6678c45f
celogeek <me@celogeek.com>
2012-07-30 00:13:46 +0200

    Add some example for debugging purpose

---
+++ examples/test_entends.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  b.pl
+#
+#        USAGE:  ./b.pl  
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  YOUR NAME (), 
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  29.07.2012 19:00:15
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+use v5.16;
+
+
+BEGIN
+{package mySuperOpt;
+    use Moo;
+    use MooX::Options;
+
+    option 't1' => ( is => 'rw', doc => 't1' );
+    1;
+}
+;
+{package myOpt;
+    use Moo;
+    use MooX::Options;
+    extends 'mySuperOpt';
+
+    option 't2' => ( 'is' => 'rw', doc => 't2' );
+    1;
+};
+
+#mySuperOpt->new_with_options;
+#myOpt->new_with_options;
+my $a = myOpt->new_with_options(t1 => 1, t2 => 2);
+say $a->t1;
+say $a->t2;
+
