8d820b5d64f8fd7b8e6670e0f9cbdda5d7094c28
celogeek <me@celogeek.com>
2013-11-13 00:21:28 +0100

    split parse_options, save sub_commands

--- lib/MooX/Options/Role.pm
+++ lib/MooX/Options/Role.pm
@@ -21,6 +21,7 @@ use JSON;
 use Carp;
 use Pod::Usage qw/pod2usage/;
 use Path::Class;
+use feature 'state';
 
 requires qw/_options_data _options_config/;
 
@@ -36,19 +37,32 @@ sub new_with_options {
     my ( $class, %params ) = @_;
     
     #save subcommand
-    # TODO The Around should be call from MooX::Cmd
     
-    # my $command_chain = delete $params{command_chain};
-    # my $command_commands = delete $params{command_commands};
+    if (ref (my $command_chain = delete $params{command_chain}) eq 'ARRAY') {
+        $class->can('around')->(
+             _options_prog_name => sub {
+                my $prog_name = Getopt::Long::Descriptive::prog_name;
+                for my $cmd (@$command_chain) {
+                    next if !ref $cmd;
+                    next if !UNIVERSAL::can($cmd,'isa');
+                    next if !$cmd->can('command_name');
+                    if (defined (my $cmd_name = $cmd->command_name)) {
+                        $prog_name .= ' ' . $cmd_name;
+                    }
+                }
+                
+                return $prog_name;    
+             }
+         );
+    }
 
-    # if (defined ($command_chain // $command_commands)) {
-    #     $class->can('around')->(
-    #         _options_config => sub {
-    #             my ( $orig, $self ) = ( shift, shift );
-    #             return ( $self->$orig(@_), command_chain => $command_chain, command_commands => $command_commands );
-    #         }
-    #     );
-    # }
+    if (ref (my $command_commands = delete $params{command_commands}) eq 'HASH') {
+        $class->can('around')->(
+             _options_sub_commands => sub {
+                return [sort keys %$command_commands];
+             }
+         );
+    }
 
     my %cmdline_params = $class->parse_options(%params);
 
@@ -87,122 +101,39 @@ It is use by "new_with_options".
 
 =cut
 
-## no critic qw/Modules::ProhibitExcessMainComplexity/
 sub parse_options {
     my ( $class, %params ) = @_;
+
     my %options_data   = $class->_options_data;
     my %options_config = $class->_options_config;
-    my @skip_options;
-    @skip_options = @{ $options_config{skip_options} }
-      if defined $options_config{skip_options};
-    if (@skip_options) {
-        delete @options_data{@skip_options};
+    if (defined $options_config{skip_options}) {
+        delete @options_data{@{$options_config{skip_options}}};
     }
-    my @options;
 
-    my $option_name = sub {
-        my ( $name, %data ) = @_;
-        my $cmdline_name = $name;
-        $cmdline_name .= '|' . $data{short} if defined $data{short};
-
-        #dash name support
-        my $dash_name = $name;
-        $dash_name =~ tr/_/-/;
-        if ( $dash_name ne $name ) {
-            $cmdline_name .= '|' . $dash_name;
-        }
-
-        $cmdline_name .= '+' if $data{repeatable} && !defined $data{format};
-        $cmdline_name .= '!' if $data{negativable};
-        $cmdline_name .= '=' . $data{format} if defined $data{format};
-        return $cmdline_name;
-    };
-
-    my %has_to_split;
-    for my $name (
-        sort {
-            $options_data{$a}{order}
-              <=> $options_data{$b}{order}    # sort by order
-              or $a cmp $b                    # sort by attr name
-        } keys %options_data
-      )
-    {
-        my %data = %{ $options_data{$name} };
-        my $doc  = $data{doc};
-        $doc = "no doc for $name" if !defined $doc;
-
-        push @options, [ $option_name->( $name, %data ), $doc ];
-        if ( defined $data{autosplit} ) {
-            $has_to_split{"--${name}"} = Data::Record->new(
-                { split => $data{autosplit}, unless => $RE{quoted} } );
-            if ( my $short = $data{short} ) {
-                $has_to_split{"-${short}"} = $has_to_split{"--${name}"};
-            }
-            for ( my $i = 1; $i < length($name); $i++ ) {
-                my $long_short = substr( $name, 0, $i );
-                $has_to_split{"--${long_short}"} = $has_to_split{"--${name}"};
-            }
-        }
-    }
+    my ($options, $has_to_split) = $class->_options_prepare_descriptive(\%options_data);
 
     local @ARGV = @ARGV if $options_config{protect_argv};
-    if (%has_to_split) {
-        my @new_argv;
-
-        #parse all argv
-        for my $i ( 0 .. $#ARGV ) {
-            my $arg = $ARGV[$i];
-            my ( $arg_name, $arg_values ) = split( /=/x, $arg, 2 );
-            unless ( defined $arg_values ) {
-                $arg_values = $ARGV[ ++$i ];
-            }
-            if ( my $rec = $has_to_split{$arg_name} ) {
-                foreach my $record ( $rec->records($arg_values) ) {
-
-                    #remove the quoted if exist to chain
-                    $record =~ s/^['"]|['"]$//gx;
-                    push @new_argv, $arg_name, $record;
-                }
-            }
-            else {
-                push @new_argv, $arg;
-            }
-        }
-        @ARGV = @new_argv;
-    }
+    @ARGV = $class->_options_split_with($has_to_split) if %$has_to_split;
 
     my @flavour;
     if ( defined $options_config{flavour} ) {
         push @flavour, { getopt_conf => $options_config{flavour} };
     }
 
-    my $prog_name = Getopt::Long::Descriptive::prog_name;
-    # support of MooX::Cmd
-    if ( ref $options_config{command_chain} eq 'ARRAY' ) {
-        for my $cmd (@{$options_config{command_chain}}) {
-            next if !ref $cmd;
-            next if !UNIVERSAL::can($cmd,'isa');
-            next if !$cmd->can('command_name');
-            if (defined (my $cmd_name = $cmd->command_name)) {
-                $prog_name .= ' ' . $cmd_name;
-            }
-        }
-    }
-    
+    my $prog_name = $class->_options_prog_name();
+
     # create usage str
     my $usage_str = "USAGE: $prog_name %o";
 
     my ( $opt, $usage ) = describe_options(
-        ($usage_str), @options,
+        ($usage_str), @$options,
         [ 'help|h', "show this help message" ],
         [ 'man', "show the manual" ],
         ,@flavour
     );
 
     $usage->{prog_name} = $prog_name;
-    if (ref $options_config{command_commands} eq 'HASH') {
-        $usage->{sub_commands} = [sort keys %{$options_config{command_commands}}];
-    }
+    $usage->{sub_commands} = $class->_options_sub_commands();
 
     my %cmdline_params = %params;
     for my $name ( keys %options_data ) {
@@ -285,5 +216,88 @@ sub options_man {
     exit(0);
 }
 
+sub _option_name {
+    my ( $class, $name, %data ) = @_;
+    my $cmdline_name = $name;
+    $cmdline_name .= '|' . $data{short} if defined $data{short};
+    #dash name support
+    my $dash_name = $name;
+    $dash_name =~ tr/_/-/;
+    if ( $dash_name ne $name ) {
+        $cmdline_name .= '|' . $dash_name;
+    }
+    $cmdline_name .= '+' if $data{repeatable} && !defined $data{format};
+    $cmdline_name .= '!' if $data{negativable};
+    $cmdline_name .= '=' . $data{format} if defined $data{format};
+    return $cmdline_name;
+}
 
+sub _options_prepare_descriptive {
+    my ($class, $options_data) = @_;
+
+    my @options;
+    my %has_to_split;
+
+    for my $name (sort {
+            $options_data->{$a}{order} <=> $options_data->{$b}{order}    # sort by order
+              or $a cmp $b                                           # sort by attr name
+        } keys %$options_data
+      )
+    {
+        my %data = %{ $options_data->{$name} };
+        my $doc  = $data{doc};
+        $doc = "no doc for $name" if !defined $doc;
+
+        push @options, [ $class->_option_name( $name, %data ), $doc ];
+        
+        if ( defined $data{autosplit} ) {
+            $has_to_split{"--${name}"} = Data::Record->new(
+                { split => $data{autosplit}, unless => $RE{quoted} } );
+            if ( my $short = $data{short} ) {
+                $has_to_split{"-${short}"} = $has_to_split{"--${name}"};
+            }
+            for ( my $i = 1; $i < length($name); $i++ ) {
+                my $long_short = substr( $name, 0, $i );
+                $has_to_split{"--${long_short}"} = $has_to_split{"--${name}"};
+            }
+        }
+    }
+
+    return \@options, \%has_to_split;
+}
+
+sub _options_split_with {
+    my ($class, $has_to_split) = @_;
+
+    my @new_argv;
+    #parse all argv
+    for my $i ( 0 .. $#ARGV ) {
+        my $arg = $ARGV[$i];
+        my ( $arg_name, $arg_values ) = split( /=/x, $arg, 2 );
+        unless ( defined $arg_values ) {
+            $arg_values = $ARGV[ ++$i ];
+        }
+        if ( my $rec = $has_to_split->{$arg_name} ) {
+            foreach my $record ( $rec->records($arg_values) ) {
+                #remove the quoted if exist to chain
+                $record =~ s/^['"]|['"]$//gx;
+                push @new_argv, $arg_name, $record;
+            }
+        }
+        else {
+            push @new_argv, $arg;
+        }
+    }
+    
+    return @new_argv;
+
+}
+
+sub _options_prog_name {
+    Getopt::Long::Descriptive::prog_name;
+}
+
+sub _options_sub_commands {
+    return;
+}
 1;
