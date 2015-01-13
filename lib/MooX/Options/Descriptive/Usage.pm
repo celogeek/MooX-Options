669fe463d98dedea7c9f40c98ed798bf21c6d0ca
Celogeek <me@celogeek.com>
2015-01-13 16:17:17 +0100

    handle possible miss of Term::Any::Size

--- lib/MooX/Options/Descriptive/Usage.pm
+++ lib/MooX/Options/Descriptive/Usage.pm
@@ -15,9 +15,16 @@ use warnings;
 # VERSION
 use feature 'say', 'state';
 use Text::LineFold;
-use Term::Size::Any qw/chars/;
 use Getopt::Long::Descriptive;
 use Scalar::Util qw/blessed/;
+
+BEGIN {
+   eval "use Term::Size::Any qw/chars/";
+   if ($@) {
+	   no strict 'refs';
+	   *{"MooX::Options::Descriptive::Usage::chars"} = sub {return (80,25)};
+   }
+}
 
 my %format_doc = (
     's'  => 'String',
