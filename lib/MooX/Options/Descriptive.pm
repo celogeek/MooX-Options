fce5a36ccfef2214a2b1ed5ebc7aef1997401c08
Celogeek <me@celogeek.com>
2015-02-13 08:12:10 +0100

    update deps

--- lib/MooX/Options/Descriptive.pm
+++ lib/MooX/Options/Descriptive.pm
@@ -12,7 +12,7 @@ use strict;
 use warnings;
 # VERSION
 
-use Getopt::Long 2.38;
+use Getopt::Long 2.43;
 use Getopt::Long::Descriptive 0.099;
 use MooX::Options::Descriptive::Usage;
 use parent 'Getopt::Long::Descriptive';
