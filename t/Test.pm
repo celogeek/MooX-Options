02b8254026a86acd0ebbe9c77db3d1b64d159897
Celogeek <me@celogeek.com>
2015-11-11 17:38:37 +0100

    setlocale for unix system

--- t/Test.pm
+++ t/Test.pm
@@ -3,9 +3,11 @@ use strict;
 use warnings;
 use Test::More;
 use Import::Into;
+use POSIX qw/setlocale LC_ALL/;
 
 sub import {
     $ENV{LC_ALL} = 'C';
+    setlocale(LC_ALL, 'C');
     my $target = caller;
     strict->import::into($target);
     warnings->import::into($target);
