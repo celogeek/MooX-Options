91bae01079a6930b0f66249b4cac6eca99ed917a
intrigeri <intrigeri@boum.org>
2019-10-14 09:20:15 +0000

    Adjust for Role-Tiny 2.001003: test suite and namespace::clean compatibility documentation
    Upgrading `Role-Tiny` from 2.001001 to 2.001003 breaks a test case:
    
    	t/16-namespace_clean.t ......................
    	ok 1 - TestNamespaceClean is a package
    	Can't locate object method "__" via package "TestNamespaceClean" at /usr/share/perl5/MooX/Options/Role.pm line 352.
    
    One way to fix this is to ensure `namespace::clean` does not hide the methods
    exported by `MooX::Locale::Passthrough`, as this commit does. But this problem
    might be the symptom of a regression in `Role-Tiny`, that perhaps we should not
    just paper over.

--- t/lib/TestNamespaceClean.pm
+++ t/lib/TestNamespaceClean.pm
@@ -1,7 +1,7 @@
 package TestNamespaceClean;
 use Moo;
 use MooX::Options;
-use namespace::clean -except => [qw/_options_data _options_config/];
+use namespace::clean -except => [qw/_options_data _options_config __ __n __p/];
 
 option foo => ( is => 'ro', format => 's' );
 
